4158c00fbd72ab89c44b46f9b60dd212493624f0
Alan Ng <alan.ng@blake.com.au>
2019-06-05 00:14:27 +1000

    prevent click propagation

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -18,11 +18,13 @@ export class ArticleComponent implements OnInit {
   }
 
   voteUp() {
-    this.votes += 1;    
+    this.votes += 1;
+    return false;
   }
 
   voteDown() {
     this.votes -= 1;
+    return false;
   }
 
   ngOnInit() {
